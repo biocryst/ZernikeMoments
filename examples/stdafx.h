f47a2094afc254e81883019ff2532645831c7573
KernelA <KernelA@users.noreply.github.com>
2020-01-18 13:02:06 +0300

    Precompiled header for MSVS added

---
+++ examples/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+
+#include "ZernikeDescriptor.h"
